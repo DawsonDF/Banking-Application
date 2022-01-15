0f61279451a20b1bd984d2e3d0909ccaacabbdc1
Dawson Freeman <rudydog56@gmail.com>
2022-01-15 15:14:44 -0600

    Created logIn and createUser method, and deleted CreateUser class. Added "data.txt" to store user information.

--- DAO.java
+++ DAO.java
@@ -30,4 +30,13 @@ public class DAO {
   public void setId(String id){
     this.id = id;
   }
+
+  public DAO(String username, String firstName, String lastName, String password){
+
+    this.username = username;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.password = password;
+
+  }
 }
--- run.java
+++ run.java
@@ -1,7 +1,43 @@
+import java.util.*;
 
-public class Main {
+public class Run {
 	public static void main (String [] args) {
-		
-	}
 
-}
+		DAO userInfo = new DAO();
+
+		Scanner scan = new Scanner (System.in);
+
+		String temp = scan.nextLine();
+
+		if (temp.equals("C")){
+
+			}
+			if (temp.equals("L")){
+
+					}
+			}
+
+
+
+
+		public void createUser () {
+			Scanner scan = new Scanner (System.in);
+
+			System.out.println("Please enter a username");
+
+			String username = scan.nextLine();
+
+			System.out.println("Please enter your first name");
+
+			String firstName = scan.nextLine();
+
+			System.out.println("Please enter your last name");
+
+			String lastName = scan.nextLine();
+
+			System.out.println("Please enter a password ");
+
+			String password = scan.nextLine();
+
+			}
+	}
