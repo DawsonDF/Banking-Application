621d44c9060618f49a6aba9382eb615891cec9b5
Dawson Freeman <rudydog56@gmail.com>
2022-01-14 21:30:46 -0600

    Created getters and setters for class fields

--- DAO.java
+++ DAO.java
@@ -1,4 +1,33 @@
 
 public class DAO {
 
+  private String username;
+  private String firstName;
+  private String lastName;
+  private String id;
+
+  public String getUsername () {
+    return this.username;
+  }
+  public void setUsername(String username){
+    this.username = username;
+  }
+  public String getFirstName () {
+    return this.firstName;
+  }
+  public void setFirstName(String firstName){
+    this.firstName = firstName;
+  }
+  public String getLastName () {
+    return this.lastName;
+  }
+  public void setLastName(String lastName){
+    this.lastName = lastName;
+  }
+  public String getId () {
+    return this.id;
+  }
+  public void setId(String id){
+    this.id = id;
+  }
 }
